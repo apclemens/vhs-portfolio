812a4449d3ae15e8c1e02a448daf1ff42508b652
Andrew Clemens <wikipedianerd@gmail.com>
2018-03-29 11:31:52 -0400

    delete unnecessary functions

--- resources/js/transitions.js
+++ resources/js/transitions.js
@@ -10,25 +10,6 @@ window.onpopstate = function(e) {
     }
 };
 
-function xtransition_to(page, index, newurl, newtitle, setstate) {
-    document.title = newtitle;
-    if (setstate) {
-        window.history.pushState({
-            "page": page,
-            "index": index,
-            "newurl": newurl,
-            "newtitle": newtitle
-        }, "", newurl);
-    }
-    if ($('#non-links').height() == 0) {
-        // transition over to new thing
-        transition_over(page, index);
-    } else {
-        // pull up header and show things
-        transition_up(page, index);
-    }
-}
-
 function create_section_div(page, top, left, width, height, backtop, backleft) {
 	var div = document.createElement('div');
 	div.classList.add('section');
@@ -78,7 +59,7 @@ function transition_to_websites(setstate) {
         y: -0.8,
         z: 0.2
     }, 1000).easing(TWEEN.Easing.Linear.None).start();
-	create_section_div('websites', '70%', '50%', '60%', '50%', '25%', '25%');
+	create_section_div('websites', '70%', '50%', '60%', '50%', '42.5%', '50%');
 }
 
 function transition_to_projects(setstate) {
@@ -163,112 +144,6 @@ function transition_to_home(setstate) {
     while (list[0]){
         list[0].parentNode.removeChild(list[0]);
     }
-}
-
-function transition_over(page, index) {
-    var prev_index = $('.section-page')[0].id.replace('section', '');
-    var left;
-    if (prev_index == index) {
-        return;
-    } else if (prev_index > index) {
-        left = -$('#content').outerWidth();
-    } else {
-        left = $('#content').outerWidth();
-    }
-    $("#links>li:nth-child(" + (+prev_index + 1) + ")").removeClass('current');
-    $('#section').append('<div class="section-page" id="section' + index + '" style="left: ' + left + 'px"></div>');
-    $('#section' + index).load(page);
-
-    $('.section-page').animate({
-        'left': '-=' + left
-    }, 500);
-    $("#links>li:nth-child(" + (index + 1) + ")").addClass('current');
-    $('#stem').animate({
-        'left': '' + (25 * index + 12.5) + '%'
-    }, 500, function() {
-        $('#section' + prev_index).remove();
-        $('.section-page').css('left', 0);
-        if (index != 3) set_scroll();
-    });
-}
-
-function transition_up(page, index) {
-    $('#section').append('<div class="section-page" id="section' + index + '"></div>');
-    $('#section' + index).css('top', -500);
-    $('#section' + index).load(page);
-
-    $('#non-links').animate({
-        'height': 0
-    }, 250);
-    setTimeout(function() {
-        $('#stem').css({
-            'opacity': 1,
-            'left': '' + (25 * index + 12.5) + '%'
-        });
-        $("#links>li:nth-child(" + (index + 1) + ")").addClass('current');
-        $('#stem').animate({
-            'height': 45
-        }, 250)
-    }, 250);
-    setTimeout(function() {
-        $('#section').animate({
-            'height': 250
-        }, 250);
-        $('#section').css({
-            'opacity': 1
-        });
-    }, 500);
-    setTimeout(function() {
-        $('#section' + index).animate({
-            'top': 0
-        }, 250)
-    }, 750);
-    setTimeout(function() {
-        if (index != 3) set_scroll();
-    }, 1100);
-}
-
-function transition_down(setstate) {
-    if (document.title == 'andrew clemens') {
-        return;
-    }
-    document.title = 'andrew clemens';
-    if (setstate) {
-        window.history.pushState({
-            "newtitle": 'andrew clemens'
-        }, "", '/');
-    }
-    setTimeout(function() {
-        $('.section-page').animate({
-            'top': -250
-        }, 250);
-    }, 0);
-    setTimeout(function() {
-        $('#section').animate({
-            'height': 0
-        }, 250);
-    }, 250);
-    setTimeout(function() {
-        $("#links>li").removeClass('current');
-        $('#stem').animate({
-            'height': 0
-        }, 250);
-        $('#section').css({
-            'opacity': 0
-        });
-    }, 500);
-    setTimeout(function() {
-        $('#non-links').animate({
-            'height': 250
-        }, 250);
-        $('#stem').css({
-            'opacity': 0
-        });
-    }, 750);
-
-    setTimeout(function() {
-        $('.section-page').remove()
-    }, 1000);
 }
 
 function transition_open_front() {
